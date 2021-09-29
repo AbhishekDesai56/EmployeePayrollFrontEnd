4729cd81dabb6fbfe4d3f5c737afb1158534511c
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-29 08:15:04 +0530

    refactor: add error div in login component

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -14,6 +14,7 @@ function LoginForm({Login, error }) {
     <form onSubmit = {submitHandler}>
         <div className="form-inner">
           <h2>Employee Login</h2>
+          {(error !== "") ? (<div className="error">{error}</div>) : ""}
             <div className="form-group">
                 <label htmlFor="email">Email:</label>
                 <input type="email" name="email" id="email" onChange = {e => setDetails({...details, email: e.target.value})} value={details.email} />
