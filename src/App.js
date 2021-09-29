15621388cd4f3c3bede1c60fda4dec8b7959d2d9
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-29 07:48:53 +0530

    refactor: added loginform component with login and logout method

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import LoginForm from './components/Login/LoginForm';
 
 function App() {
+  const emploeeLogin = {
+    name:"Test",
+    email: "Test@gmail.com",
+    password:"Test@123"
+  }
+
+  const [user, setUser] = useState({email:"", name: ""});
+  const [error, setError] = useState("");
+  
+
+  const Login = (loginDetails) => {
+    if(emploeeLogin.email === loginDetails.email || emploeeLogin.password === loginDetails.password) {
+      console.log("Successfully Login In");
+      setUser({
+        name: emploeeLogin.name,
+        email: loginDetails.email
+      })
+    } else {
+      console.log("Invalid Credintials");
+    }
+  }
+
+  const Logout = () => {
+    setUser({
+      name: "",
+      email: ""
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email !== "") ? (
+        <div className= "welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login = {Login} error={error} />
+      )}
     </div>
   );
 }
