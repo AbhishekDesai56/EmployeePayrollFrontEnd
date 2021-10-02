7c92e6b4d79fa2101db7db272e16f13b20847ec4
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-10-02 19:15:13 +0530

    refactor: design register form using material ui

--- src/components/UserRegisteration/UserRegisterationForm.js
+++ src/components/UserRegisteration/UserRegisterationForm.js
@@ -1,89 +1,31 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import React from 'react'
+import {Grid, Paper, Avatar,TextField, Button, Link, Typography} from "@material-ui/core";
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 const UserRegistrationForm = () => {
-  // Note that we have to initialize ALL of fields with values. These
-  // could come from props, but since we don’t want to prefill this form,
-  // we just use an empty string. If we don’t do this, React will yell
-  // at us.
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-    },
-    validationSchema: Yup.object({
-        firstName: Yup.string().required('Required'),
-        lastName: Yup.string().required('Required'),
-        email: Yup.string().email('Invalid email address').required('Required'),
-        password: Yup.string().required('Password is required'),
-        confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Password is not matching')
-    }),
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+  const paperStyle = {padding: 20, height:'90vh',width:280,margin:'20px auto'}
+  const avatarStyle = {backgroundColor: '#1bbd7e' };
+  const headerStyle = {margin: '0px'};
+  const btnStyle = {margin: '8px 0'}
   return (
-    <form onSubmit={formik.handleSubmit}>
-        <div className="form-inner">
-             <h2>Employee Registration</h2>
-             <div className="form-group">
-                <label htmlFor="firstName">First Name</label>
-                <input
-                    id="firstName"
-                    name="firstName"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.firstName}
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="lastName">Last Name</label>
-                <input
-                    id="lastName"
-                    name="lastName"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.lastName}
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Confirm Password</label>
-                <input
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    type="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.confirmPassword}
-                />
-            </div>
-            <button type="submit">Register</button>
-        </div>
-    </form>
-  );
-};
+    <Grid align='center'>
+        <Paper elevation={10} style={paperStyle}>
+            <Grid>
+            <Avatar style={avatarStyle}><AddCircleOutlineOutlinedIcon/></Avatar>
+            <h2 style={headerStyle}>Sign Up</h2>
+            <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
+            </Grid>
+            <form>
+                <TextField label="First Name" placeholder="Enter your First Name" variant="outlined" margin="normal" fullWidth required/>
+                <TextField label="Last Name" placeholder="Enter your Last Name" variant="outlined" margin="normal" fullWidth required/>
+                <TextField label="Email" placeholder="Enter your Email Id" variant="outlined" margin="normal" fullWidth required/>
+                <TextField label="Password" placeholder="Enter your Password" type="password"  variant="outlined" margin="normal" fullWidth required/>
+                <TextField label="Confirm Password" placeholder="Enter your Confirm Password" type="password"  variant="outlined" margin="normal" fullWidth required/>
+                <Button type="submit" style={btnStyle} color="primary" variant="contained" fullWidth>Sign In</Button>
+            </form>
+        </Paper>
+    </Grid>
+    )
+}
 
-export default UserRegistrationForm
+export default UserRegistrationForm;
