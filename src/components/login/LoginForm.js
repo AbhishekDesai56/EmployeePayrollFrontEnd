3c7b30472f542115f43e0fc74eaf13606873a689
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-10-02 18:31:14 +0530

    refactor: design a form using material ui

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,42 +1,28 @@
 import React from 'react'
-import { useState } from 'react/cjs/react.development'
- import UserRegistrationForm from "../UserRegisteration/UserRegisterationForm";
-import { BrowserRouter, Link, Route  } from 'react-router-dom';
+import {Grid, Paper, Avatar,TextField, Button, Link, Typography} from "@material-ui/core";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
-function LoginForm({Login, error }) {
-  const [details, setDetails] = useState({email: "", password: ""});
-  
-  const submitHandler = e => {
-    e.preventDefault();
-
-    Login(details);
-  }
-
+const LoginForm = () => {
+  const paperStyle = {padding: 20, height:'70vh',width:280,margin:'20px auto'}
+  const avatarStyle = {backgroundColor: '#1bbd7e' };
+  const btnStyle = {margin: '8px 0'}
   return (
-    <form onSubmit = {submitHandler}>
-        <div className="form-inner">
-          <h2>Employee Login</h2>
-          {(error !== "") ? (<div className="error">{error}</div>) : ""}
-            <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input type="email" name="email" id="email" onChange = {e => setDetails({...details, email: e.target.value})} value={details.email} />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input type="password" name="password" id="password" onChange = {e => setDetails({...details, password: e.target.value})} value={details.password}/>
-            </div>
-            <input type="submit" value="Login"></input>
-            <BrowserRouter>
-              <Link to = "../UserRegisteration/UserRegisterationForm">SignUp</Link>
-              <Route
-                    path="/UserRegisteration/UserRegisterationForm"
-                    component={UserRegistrationForm}
-                    exact 
-                />
-            </BrowserRouter>
-        </div>
-    </form>
+    <Grid align='center'>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid>
+          <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+          <h2>Sign In</h2>
+        </Grid>
+          <TextField label="Email" placeholder="Enter your Email Id" variant="outlined" margin="normal" fullWidth required/>
+          <TextField label="Password" placeholder="Enter your Password" type="password"  variant="outlined" margin="normal" fullWidth required/>
+          <Button type="submit" style={btnStyle} color="primary" variant="contained" fullWidth>Sign In</Button>
+          <Typography> Do you have an account?
+            <Link href="#">Sign Up</Link>
+          </Typography>
+      </Paper>
+    </Grid>
   )
 }
 
-export default LoginForm
+export default LoginForm;
+  
