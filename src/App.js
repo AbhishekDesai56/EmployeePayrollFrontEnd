e2769c92e23e29ee7d5c7cd7ce49f5e79870f555
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-10-04 02:07:08 +0530

    refactor: define routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
-
 import './App.scss';
-import { useState } from 'react';
 import LoginForm from './components/Login/LoginForm';
+import UserRegistrationForm from './components/UserRegisteration/UserRegisterationForm';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
-  const emploeeLogin = {
-    name:"Test",
-    email: "Test@gmail.com",
-    password:"Test@123"
-  }
-
-  const [user, setUser] = useState({email:"", name: ""});
-  const [error, setError] = useState("");
-  
-
-  const Login = (loginDetails) => {
-    if(emploeeLogin.email === loginDetails.email || emploeeLogin.password === loginDetails.password) {
-      console.log("Successfully Login In");
-      setUser({
-        name: emploeeLogin.name,
-        email: loginDetails.email
-      })
-    } else {
-      console.log("Invalid Credintials");
-      setError("Invalid Credintials");
-    }
-  }
-
-  const Logout = () => {
-    setUser({
-      name: "",
-      email: ""
-    })
-  }
-
   return (
+    <Router>
     <div className="App">
-      {(user.email !== "") ? (
-        <div className= "welcome">
-          <h2>Welcome, <span>{user.name}</span></h2>
-          <button onClick={Logout}>Logout</button>
-        </div>
-      ) : (
-        <LoginForm Login = {Login} error={error} />
-      )}
+      <Switch>
+        <Route path='/UserRegisteration/' exact component={UserRegistrationForm}></Route>
+        <Route path='/'>
+          <LoginForm />  
+        </Route> 
+      </Switch>
     </div>
+    </Router>
   );
 }
 
