7f356f4bc2a1305e3c2a30477a32d785dd10d889
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-10-06 07:38:59 +0530

    refactor: add routes for dashboard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.scss';
 import LoginForm from './components/Login/LoginForm';
 import UserRegistrationForm from './components/UserRegisteration/UserRegisterationForm';
+import Dashboard from './components/Dashboard/dashboard';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Switch>
-        <Route path='/UserRegisteration/' exact component={UserRegistrationForm}></Route>
-        <Route path='/'>
-          <LoginForm />  
-        </Route> 
-      </Switch>
-    </div>
+      <div className="App">
+          <Switch>
+            <Route path='/UserRegisteration/' exact component={UserRegistrationForm}></Route>
+            <Route path='/Dashboard/' exact component={Dashboard}></Route>
+            <Route path='/'>
+              <LoginForm />  
+            </Route> 
+          </Switch>
+      </div>
     </Router>
   );
 }
