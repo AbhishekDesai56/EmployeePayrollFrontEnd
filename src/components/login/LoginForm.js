aee315d8e5a3eb93e5cb67f9bd9bb7237a8c3096
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-10-06 07:33:32 +0530

    refactor: Validating the user credentials

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -3,24 +3,60 @@ import {Grid, Paper, Avatar,TextField, Button, Typography,Link} from "@material-
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import {Formik,Form,Field, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
+import { useHistory } from "react-router-dom";
+import EmployeeService from '../../services/employee.service';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LoginForm = () => {
   const paperStyle = {padding: 20, height:'70vh',width:280,margin:'20px auto'}
   const avatarStyle = {backgroundColor: '#1bbd7e' };
   const btnStyle = {margin: '8px 0'}
+  const history = useHistory()
   const initialValues = {
     email:'',
     password:''
   }
 
   const onSumbit=(values,props) => {
-    console.log(values);
+        let data = {
+          email: values.email,
+          password: values.password
+        }
+       EmployeeService.login(data).then(response => {
+         if(response.data.success === true) {
+           setTimeout(() => {
+              history.push("/Dashboard");
+            }, 2000);
+          toast.success(response.data.message, {
+                        position: "bottom-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        });
+            } 
+       }).catch((error) => {
+          toast.error('Invalid Credintials', {
+                        position: "bottom-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        });
+       });
   }
 
   const validationSchema = Yup.object().shape({
     email: Yup.string().email('Enter a email').required(),
     password: Yup.string().required('Enter a password')
-  })
+  });
+
+
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
@@ -29,17 +65,16 @@ const LoginForm = () => {
           <h2>Sign In</h2>
         </Grid>
         <Formik initialValues={initialValues} onSubmit={onSumbit} validationSchema={validationSchema}>
-         
           {(props)=> (
             <Form>
-              {console.log(props)}
               <Field as={TextField} label="Email" name="email" 
-              placeholder="Enter your Email Id" variant="outlined" margin="normal" fullWidth
-              helperText={<ErrorMessage name="email"/>}
+                placeholder="Enter your Email Id" variant="outlined" margin="normal" fullWidth
+                helperText={<ErrorMessage name="email"/>}
               />
               <Field as={TextField} label="Password" name="password" 
-              placeholder="Enter your Password" type="password"  variant="outlined" margin="normal" fullWidth
-              helperText={<ErrorMessage name="password"/>}/>
+                placeholder="Enter your Password" type="password"  variant="outlined" margin="normal" fullWidth
+                helperText={<ErrorMessage name="password"/>}
+              />
               <Button type="submit" style={btnStyle} color="primary" variant="contained" fullWidth>Sign In</Button>
             </Form>
           )}
@@ -47,6 +82,17 @@ const LoginForm = () => {
         <Typography> Do you have an account?
           <Link href="/UserRegisteration/">Sign Up</Link>
         </Typography>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </Paper>
     </Grid>
   )
