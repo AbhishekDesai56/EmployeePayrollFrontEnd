d8b875df5b29894e8e99a2899e4328d4428006ac
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-10-10 13:48:17 +0530

    refactor: added getAllEmployees method

--- src/services/employee.service.js
+++ src/services/employee.service.js
@@ -1,13 +1,22 @@
-import http from '../http-common.js';
+import http from "../http-common.js";
+let token = sessionStorage.getItem("token");
+
+const header = {
+  headers: { token: token },
+};
 
 class EmployeeService {
-    login(data) {
-        return http.post('/login',data);
-    }
+  login(data) {
+    return http.post("/login", data);
+  }
 
-    register(data) {
-        return http.post('/register',data);
-    }
+  register(data) {
+    return http.post("/register", data);
+  }
+
+  getAllEmployees = () => {
+    return http.get("/getEmployees", header);
+  };
 }
 
 export default new EmployeeService();
